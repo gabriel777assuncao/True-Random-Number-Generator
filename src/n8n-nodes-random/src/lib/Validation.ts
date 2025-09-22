0c041eecb56e309faf6bd8fbae914555f695d18e
gassuncao <gassuncao@symplicity.com>
2025-09-22 14:51:21 -0300

    feat: creating a way to generate random number

--- src/n8n-nodes-random/src/lib/Validation.ts
+++ src/n8n-nodes-random/src/lib/Validation.ts
@@ -3,11 +3,13 @@ export function assertNumber(name: string, v: unknown) {
 		throw new Error(`${name} must be a valid number.`);
 	}
 }
+
 export function assertInteger(name: string, v: number) {
 	if (!Number.isInteger(v)) {
 		throw new Error(`${name} must be an integer.`);
 	}
 }
+
 export function assertMinLEMax(min: number, max: number) {
 	if (min > max) {
 		throw new Error('Min cannot be greater than Max.');
